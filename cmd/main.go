5cce8ab6070a16238dacbb7df0fe037ff196bcae
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2025-12-26 12:08:42 +0100

    Add parsing positional arguments

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+
+	gocql "github.com/apache/cassandra-gocql-driver/v2"
 )
+
+var voteQuery *gocql.Query
 
 type Candidate struct {
 	CandidateID int    `json:"candidate_id"`
@@ -19,7 +24,7 @@ type Party struct {
 }
 
 type District struct {
-	DistrictID string  `json:"district_id"`
+	DistrictID int     `json:"district_id"`
 	Name       string  `json:"name"`
 	Mandates   int     `json:"mandates"`
 	Parties    []Party `json:"parties"`
@@ -30,11 +35,68 @@ type Election struct {
 	Districts  []District `json:"districts"`
 }
 
+type Identifiable interface {
+	GetID() int
+	GetName() string
+}
+
+func (d District) GetID() int      { return d.DistrictID }
+func (d District) GetName() string { return d.Name }
+
+func (p Party) GetID() int      { return p.PartyID }
+func (p Party) GetName() string { return p.PartyName }
+
+func (c Candidate) GetID() int      { return c.CandidateID }
+func (c Candidate) GetName() string { return c.Name }
+
+func parseArgToItem[T Identifiable](arg string, items []T) (T, int, bool) {
+	var zero T
+	id, err := strconv.Atoi(arg)
+	if err == nil {
+		for _, item := range items {
+			if item.GetID() == id {
+				return item, id, true
+			}
+		}
+	} else {
+		for _, item := range items {
+			if item.GetName() == arg {
+				return item, item.GetID(), true
+			}
+		}
+	}
+
+	return zero, -1, false
+}
+
+func initCassandraSession() (*gocql.Session, error) {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "elections"
+	return cluster.CreateSession()
+}
+
+func vote(districtID string, partyID int, candidateID int) error {
+	err := voteQuery.Bind(districtID, partyID, candidateID).Exec()
+	if err != nil {
+		return fmt.Errorf("failed to execute vote query: %w", err)
+	}
+	// TODO: check network partition fault here
+	return nil
+}
+
+func initialQuery(session *gocql.Session) {
+	voteQuery = session.Query(`UPDATE votes SET vote_count = vote_count + 1 WHERE district_id = ? AND party_id = ? AND candidate_id = ?`)
+}
+
 func main() {
 	filePath := flag.String("f", "", "Path to election lists json file")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <district id|string> <party id|string> <candidate id|string>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	if *filePath == "" {
+	if *filePath == "" || flag.NArg() != 3 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -67,4 +129,24 @@ func main() {
 			}
 		}
 	}
+
+	district, districtID, ok := parseArgToItem(flag.Arg(0), election.Districts)
+	if !ok {
+		fmt.Printf("Invalid district: %s\n", flag.Arg(0))
+		os.Exit(1)
+	}
+
+	party, partyID, ok := parseArgToItem(flag.Arg(1), district.Parties)
+	if !ok {
+		fmt.Printf("Invalid party: %s\n", flag.Arg(1))
+		os.Exit(1)
+	}
+	candidate, candidateID, ok := parseArgToItem(flag.Arg(2), party.Candidates)
+	if !ok {
+		fmt.Printf("Invalid candidate: %s\n", flag.Arg(2))
+		os.Exit(1)
+	}
+
+	fmt.Printf("Voting for candidate %s (ID: %d) from party %s (ID: %d) in district %s (ID: %d)\n",
+		candidate.Name, candidateID, party.PartyName, partyID, district.Name, districtID)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mati2251/dsshfs
 
 go 1.25.4
+
+require (
+	github.com/apache/cassandra-gocql-driver/v2 v2.0.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
