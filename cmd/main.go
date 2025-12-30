8010a77c6475ba87f2fee196b3398f535f41f037
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2025-12-30 21:41:36 +0100

    Add results

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,7 @@ var (
 type Candidate struct {
 	CandidateID int    `json:"candidate_id"`
 	Name        string `json:"name"`
+	Votes       int    `json:",omitempty"`
 }
 
 type Party struct {
@@ -85,6 +86,12 @@ func initCassandraSession() (*gocql.Session, error) {
 	cluster := gocql.NewCluster("172.28.0.10", "172.28.0.11", "172.28.0.12", "172.28.0.13")
 	cluster.Keyspace = "elections"
 	return cluster.CreateSession()
+}
+
+func initQuery(session *gocql.Session) {
+	voteQuery = session.Query(`UPDATE votes SET votes = votes + 1 WHERE district_id = ? AND party_id = ? AND candidate_id = ?`)
+	resultQuery = session.Query(`SELECT district_id, party_id, candidate_id, votes FROM votes WHERE district_id = ?`)
+	clearQuery = session.Query(`TRUNCATE votes`).Consistency(gocql.All)
 }
 
 func vote(election Election) error {
@@ -201,14 +208,70 @@ func heavyVoter(election Election, votesChan <-chan struct{}, wg *sync.WaitGroup
 	wg.Done()
 }
 
-func clearVotes() error {
-	return clearQuery.Exec()
+func results(e Election) error {
+	districtIdToIndex := make(map[int]int)
+	partyIdToIndex := make(map[int]int)
+	for i, d := range e.Districts {
+		districtIdToIndex[d.DistrictID] = i
+		if i == 0 {
+			for j, p := range d.Parties {
+				partyIdToIndex[p.PartyID] = j
+			}
+		}
+	}
+
+	for _, d := range e.Districts {
+		result := resultQuery.Bind(d.DistrictID).Iter()
+		candidateIdToIndex := make(map[int]map[int]int)
+		for _, p := range d.Parties {
+			candidateIdToIndex[p.PartyID] = make(map[int]int)
+			for j, c := range p.Candidates {
+				candidateIdToIndex[p.PartyID][c.CandidateID] = j
+			}
+		}
+		var districtID, partyID, candidateID, votes int
+		for result.Scan(&districtID, &partyID, &candidateID, &votes) {
+			pIndex := partyIdToIndex[partyID]
+			cIndex := candidateIdToIndex[partyID][candidateID]
+			e.Districts[districtIdToIndex[districtID]].Parties[pIndex].Candidates[cIndex].Votes = votes
+		}
+	}
+
+	perPartyTotals := make(map[int]int)
+	for _, d := range e.Districts {
+		for _, p := range d.Parties {
+			for _, c := range p.Candidates {
+				perPartyTotals[p.PartyID] += c.Votes
+			}
+		}
+	}
+
+	totalVotes := 0
+	for _, v := range perPartyTotals {
+		totalVotes += v
+	}
+
+	passThresholdVotes := totalVotes * voteThreshold / 100
+	isPassed := make(map[int]bool)
+
+	fmt.Printf("Total Votes: %d\n", totalVotes)
+	fmt.Println("Party Results:")
+	for partyID, votes := range perPartyTotals {
+		percentage := (float64(votes) /  float64(totalVotes)) * 100
+		fmt.Printf("Party: %d, Votes: %d, Percentage: %.2f%%\n", partyID, votes, percentage)
+		if votes >= passThresholdVotes {
+			isPassed[partyID] = true
+		} else {
+			isPassed[partyID] = false
+			fmt.Printf("Party ID %d did not pass the threshold with %d votes\n", partyID, votes)
+		}
+	}
+
+	return nil
 }
 
-func initQuery(session *gocql.Session) {
-	voteQuery = session.Query(`UPDATE votes SET votes = votes + 1 WHERE district_id = ? AND party_id = ? AND candidate_id = ?`)
-	resultQuery = session.Query(`SELECT district_id, party_id, candidate_id, votes FROM votes WHERE district_id = ?`)
-	clearQuery = session.Query(`TRUNCATE votes`).Consistency(gocql.All)
+func clearVotes() error {
+	return clearQuery.Exec()
 }
 
 func main() {
@@ -266,7 +329,6 @@ func main() {
 
 	initQuery(session)
 
-
 	cmd := flag.Arg(1)
 	switch cmd {
 	case "vote":
@@ -282,6 +344,10 @@ func main() {
 	case "clear":
 		if err := clearVotes(); err != nil {
 			fmt.Printf("Error clearing votes: %v\n", err)
+		}
+	case "results":
+		if err := results(election); err != nil {
+			fmt.Printf("Error calculating results: %v\n", err)
 		}
 	default:
 		flag.Usage()
